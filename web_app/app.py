81fd026661bc718e05e12415bb811ac778394ecd
Teosoft7 <thjeon.wa@gmail.com>
2019-12-17 16:47:10 -0800

    clean eda notebook

---
+++ web_app/app.py
@@ -0,0 +1,97 @@
+# my functions
+from functions import (get_place,
+                       get_shortest,)
+
+# import common libraries
+import pandas as pd
+from flask import Flask, request, render_template, url_for
+from pymongo import MongoClient
+
+connection = MongoClient(port=27017)
+db = connection['cp_seoul']
+app = Flask(__name__, static_url_path="")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route('/')
+def index():
+    """Return the main page."""
+    # get random 3 places for display
+    places = get_place(db, count=3)
+
+    print(get_shortest((places[0]['contentid'])))
+
+    # get total view counts and increments for last 4 hours
+    # total_view, increments = get_sum_view_count(db)
+    # stats= {
+    #     'videos': get_collection_count(db, 'video'),
+    #     'increments': get_count_string(increments),
+    #     'total_view': get_count_string(total_view)
+    # }
+
+    # return with template
+    return render_template('index.html', 
+                            count=len(places),
+                            places=places)
+
+# @app.route('/hottrend')
+# def hot_trend():
+#     """Return hot trend page."""
+#     # Need to add data set for hot trend
+#     # Hot : top 10 most increments views for last 4 hours
+#     count = 10
+#     videos = get_hot_video(db, count=count)
+    
+#     # apply format for large numbers
+#     for video in videos:
+#         video['view_count'] = get_count_string(video['view_count'])
+#         video['increment'] = get_count_string(video['increment'])
+
+#     return render_template('hot_trend.html', videos=videos, count=count)
+
+# @app.route('/mostwatched')
+# def most_watched():
+#     """Return most watched page."""
+#     # Need to add data set for most_watched
+#     # Most watched : top 10 most watched video
+#     count = 10
+#     videos = get_most_watched_video(db, count=count)
+    
+#     # apply format for large numbers
+#     for video in videos:
+#         video['view_count'] = get_count_string(video['view_count'])
+
+#     return render_template('most_watched.html', videos=videos, count=count)
+
+# @app.route('/newreleased')
+# def new_released():
+#     """Return new released page."""
+#     # Need to add data set for new released
+#     # new_released : top 6 most recently released video
+#     count = 6
+#     videos = get_most_recent_video(db, count=count)
+
+#     return render_template('new_released.html', videos=videos, count=count)
+
+@app.route('/about')
+def about():
+    """Return the about page."""
+    return render_template('about.html')
+
+@app.route('/detail', methods=['GET'])
+def detail():
+    """Return shortest places page.""" 
+
+    # get contentid
+    content_id = request.args.get('contentid')
+
+    # get shortest places
+    title, places = get_shortest(content_id)
+    print(content_id)
+    print(places)
+
+    return render_template('detail.html', 
+                            title=title,
+                            places=places, 
+                            count=len(places))
---
+++ web_app/functions.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+import time
+import pickle
+
+from datetime import datetime, timedelta
+from pymongo import MongoClient, DESCENDING
+
+def get_place(db, count=3):
+    """Return the random selected place from the db"""
+    place_coll = db['seoul_poi']
+
+    cursor = place_coll.find({'contenttypeid': '76',
+    'overview': {"$regex": "^[\s\S]{0,}$"},
+    'firstimage': {'$ne': ''}})
+    
+    places = [place for place in cursor]
+
+    idx = np.arange(len(places))
+    selected = np.random.choice(idx, count, replace=False)
+    
+    selected_place = []
+    for idx in selected:
+        place = places[idx]
+        note = "..."
+        try:
+            note = place['overview']
+            place.update({'note': note[:200] + '...'})
+        except:
+            print("Error occured when extracting note")
+
+        selected_place.append(place)
+        
+    return selected_place
+
+def get_shortest(contentid, count=3):
+    """Return shortest n vectors in matrix"""
+
+    # load dataframe and vector matrix from pickled file
+    svd_values = None
+    df = None
+    with open('poi_vectors', 'rb') as f:
+        svd_values = pickle.load(f)
+
+    with open('poi_df', 'rb') as f:
+        df = pickle.load(f)
+
+    # extract vectors to tuple array    
+    vectors = [(i[0], i[1], i[2]) for i in svd_values]
+
+    # find index of contentid
+    index = df[df['contentid'] == contentid].index[0]
+    title = df[df['contentid'] == contentid].title.values[0]
+
+    # extract value of each dimension for calculating distance
+    X = vectors[index][0]
+    Y = vectors[index][1]
+    Z = vectors[index][2]
+ 
+    distances = []
+    for i, value in enumerate(vectors):
+        distance = (abs(X - value[0]) + abs(Y - value[1]) + abs(Z - value[2]), i)
+        distances.append(distance)
+
+    top5_similars = [i[1] for i in sorted(distances)[1:count + 1]]
+
+    result = []
+    i = 0
+    for _, row in df.loc[top5_similars].iterrows():
+        item = {
+            'title': row['title'],
+            'contentid': row['contentid'],
+            'overview': row['overview'],
+            'note': row['overview'][:200] + '...',
+            'firstimage': row['firstimage'],
+            'modifiedtime': row['modifiedtime'],
+            'distance': distances[i][0]
+        }
+        result.append(item)
+        i += 1
+    
+    return title, result
+    
+
+    
+
